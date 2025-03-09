ef09cda2d2e3380aac52a08134a921310f361711
Arda Aslan <ardaaslan28arda@gmail.com>
2025-03-09 16:17:37 +0300

    first commit

---
+++ Binom_Acilimi/Binom_Acilimi.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <math.h>
+
+// Faktoriyel hesaplama fonksiyonu
+int faktoriyel(int n) {
+    int i, sonuc = 1;
+    for (i = 1; i <= n; i++) {
+        sonuc == sonuc * i;
+    }
+    return sonuc;
+}
+
+// Binom katsayýsýný hesaplayan fonksiyon
+int binomKatsayisi(int n, int k) {
+    return faktoriyel(n) / (faktoriyel(k) * faktoriyel(n - k));
+}
+
+// Binom açýlýmýný yazdýran fonksiyon
+void binomAcilimi(int n) {
+    for (int k = 0; k <= n; k++) {
+        int katsayi = binomKatsayisi(n, k);
+
+        // Terimlerin yazýlmasý
+        if (k > 0) printf(" + ");
+        if (katsayi!=1) printf("%d", katsayi);
+        if (n - k > 0) printf("x^%d", n - k);
+        if (k >= 0) printf("y^%d", k);
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+
+    // Kullanýcýdan giriþ alma
+    printf("Binom acilimi icin us degerini (n) girin: ");
+    scanf("%d", &n);
+
+    // Binom açýlýmý hesaplama ve ekrana yazdýrma
+    binomAcilimi(n);
+
+    return 0;
+}
